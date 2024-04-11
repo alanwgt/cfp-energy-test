4bdf193823d8381493eb2579d9883961c05949c1
alanwgt <hi@alanwgt.com>
2024-04-11 08:52:29 -0300

    feat: add React base files and fix hot reload inside docker

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    optimizeDeps: {
+        esbuildOptions: {
+            jsx: 'automatic',
+        },
+    },
+    esbuild: {
+        jsxInject: 'import React from "react";'
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/index.jsx'],
             refresh: true,
         }),
     ],
+    server: {
+        host: true,
+    },
 });
