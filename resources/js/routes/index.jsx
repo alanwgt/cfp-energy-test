ffa52267736c090cbe3dfd1f5f0976f27c89e868
alanwgt <hi@alanwgt.com>
2024-04-22 13:14:06 -0300

    refactor: move users route to a grouping route component

--- resources/js/routes/index.jsx
+++ resources/js/routes/index.jsx
@@ -10,8 +10,12 @@ export default function AppRoutes() {
         <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/profile' element={<UserProfilePage />} />
-            <Route path='/users' element={<ListUsersPage />} />
-            <Route path='/users/new' element={<ManageUserPage />} />
+
+            <Route path='users'>
+                <Route index element={<ListUsersPage />} />
+                <Route path=':id/edit' element={<ManageUserPage />} />
+                <Route path='/users/new' element={<ManageUserPage />} />
+            </Route>
         </Routes>
     );
 }
